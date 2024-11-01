d6e82d99177fadde6bf5581da1ab9250f9b9d324
Maksim Shcherbakov <94864283+M4RFF@users.noreply.github.com>
2024-11-01 21:45:12 +0100

    My first java code

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hey Maks!");
+        System.out.println("Hello world");
+        System.out.println("the third output");
+        System.out.println(32);
+    }
+}
