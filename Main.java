0114eb61e700c2811cba56fd3057214d9fc2e87e
Maksim Shcherbakov <94864283+M4RFF@users.noreply.github.com>
2024-11-17 13:19:59 +0100

    The beginning of java learning

--- Main.java
+++ Main.java
@@ -1,8 +1,13 @@
+package JavaCourse;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hey Maks!");
-        System.out.println("Hello world");
-        System.out.println("the third output");
-        System.out.println(32);
+
+        System.out.println("My name is Maks");
+        System.out.println("I'm 20 yo");
+        System.out.println("Auto generated"); // sout + tab
+
+
     }
 }
