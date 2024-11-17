48a3cdd1d5a21f6ddbbcdca0b250a4de30422a58
Maksim Shcherbakov <94864283+M4RFF@users.noreply.github.com>
2024-11-17 13:40:16 +0100

    lesson of variables

---
+++ Variable/variable.java
@@ -0,0 +1,30 @@
+package JavaCourse.Variable;
+
+public class variable {
+
+    public static void main(String[] args) {
+
+        int x = 20; // initialization
+        System.out.println("I'm " +x+ " yo!");
+
+        long l = 13131313131313L; // extremely long variables
+        System.out.println("My long variable is " +l);
+
+        float f = 3.13f; // float variable
+        System.out.println("My float variable is " +f);
+
+        double d = 3.13; // better to use double instead of float
+        System.out.println("My double variable is " +d);
+
+        boolean b = false; // boolean can be as false as true
+        System.out.println("My boolean variable is " +b);
+
+        char symbol = '@';
+        System.out.println("My char is " +symbol);
+
+        String name = "Maks"; // it's reference data type because we use capital letter String
+        System.out.println("My name is " +name);
+
+    }
+
+}
