af00d486fbc2539fb415baee483b5991807069a1
Maksim Shcherbakov <94864283+M4RFF@users.noreply.github.com>
2024-11-18 12:35:17 +0100

    Expressions

---
+++ Expressions/expressions.java
@@ -0,0 +1,15 @@
+package JavaCourse.Expressions;
+
+public class expressions {
+    public static void main(String[] args) {
+
+        int friends = 10;
+
+        friends++; // friends += 1
+        friends--; // friends -= 1
+
+        friends = friends / 3;
+
+        System.out.println(friends);
+    }
+}
