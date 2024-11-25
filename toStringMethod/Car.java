1fdf43819558a410e2c920dd70ee477bb5029a8d
Maksim Shcherbakov <94864283+M4RFF@users.noreply.github.com>
2024-11-25 18:01:20 +0100

    toString

---
+++ toStringMethod/Car.java
@@ -0,0 +1,16 @@
+package JavaCourse.toStringMethod;
+
+public class Car {
+
+    String make = "Benz";
+    String model = "c63AMG";
+    String color = "silver";
+    int year = 2024;
+
+    public String toString() {
+
+//        String myString = make + "\n" + model + "\n" + color + "\n" + year;
+//        return myString;
+        return  make + "\n" + model + "\n" + color + "\n" + year;
+    }
+}
---
+++ toStringMethod/tostringmethod.java
@@ -0,0 +1,15 @@
+package JavaCourse.toStringMethod;
+
+public class tostringmethod {
+
+    public static void main(String[] args) {
+
+        Car car = new Car();
+
+        System.out.println(car.toString());
+        System.out.println(car);
+
+
+    }
+
+}
