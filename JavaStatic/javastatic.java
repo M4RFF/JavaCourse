b4752a151d6e61d32dc31edbcb54ff3270bd63b1
Maksim Shcherbakov <94864283+M4RFF@users.noreply.github.com>
2024-11-26 15:48:47 +0100

    Static Modifier

---
+++ JavaStatic/javastatic.java
@@ -0,0 +1,15 @@
+package JavaCourse.JavaStatic;
+
+public class javastatic {
+
+    public static void main(String[] args) {
+
+        Friend friend1 = new Friend("Egor");
+        Friend friend2 = new Friend("Maks");
+        Friend friend3 = new Friend("Patric");
+
+        Friend.displayFriends();
+    }
+
+
+}
